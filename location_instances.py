e7e5ce4852a6a34eb9615730224deedbc95dd3fb
ianstrom <iankstrom@gmail.com>
2023-03-28 16:30:03 -0400

    abstracting moving logic, refactoring moving logic code, adding print messages for moving commands

--- location_instances.py
+++ location_instances.py
@@ -3,25 +3,24 @@ from models import Location
 
 
 
-location []
 
-l1 = Location("","","Reception Desk", "Stairs")
-l2 = Location("","", "Elevator", "Johnson Door")
-l3 = Location("Men's Bathroom","", "Stairs", "Johnson")
-l4 = Location("Johnson Kitchen", "Johnson Couches", "Johnson Door", "")
-l5 = Location("Johnson","","","")
-l6 = Location("Edith", "Johnson","","")
-l7 = Location("","Johnson Kitchen","","")
-l8 = Location("","Johnson Door","","")
-l9 = Location("","","Foyer", "Elevator")
-l10 = Location("Kitchen", "Turing", "Couches", "Reception Desk")
-l11 = Location("Foyer","","","")
-l12 = Location("Collins", "Foyer","","Kitchen")
-l13 = Location("","Couches","","")
-l14 = Location("Kay", "Foyer", "Couches","")
-l15 = Location("Phone Booths", "Kitchen","","")
-l16 = Location("Phase 5 Area", "Kay","","")
-l17 = Location("","","","Phone Booths")
+l1 = Location("","","Reception Desk: (a)", "Stairs: (d)")
+l2 = Location("","", "Elevator(a)", "Johnson Door: (d)")
+l3 = Location("Men's Bathroom: (w)","", "Stairs: (a)", "Johnson: (d)")
+l4 = Location("Johnson Kitchen: (w)", "Johnson Couches: (s)", "Johnson Door: (a)", "")
+l5 = Location("Johnson: (w)","","","")
+l6 = Location("Edith: (w)", "Johnson: (s)","","")
+l7 = Location("","Johnson Kitchen: (s)","","")
+l8 = Location("","Johnson Door: (s)","","")
+l9 = Location("","","Foyer: (a)", "Elevator: (d)")
+l10 = Location("Kitchen: (w)", "Turing: (s)", "Couches: (a)", "Reception Desk: (d)")
+l11 = Location("Foyer: (w)","","","")
+l12 = Location("Collins: (w)", "Foyer: (s)","","Kitchen: (d)")
+l13 = Location("","Couches: (s)","","")
+l14 = Location("Kay: (w)", "Foyer: (s)", "Couches: (a)","")
+l15 = Location("Phone Booths: (w)", "Kitchen: (s)","","")
+l16 = Location("Phase 5 Area: (w)", "Kay: (s)","","")
+l17 = Location("","","","Phone Booths: (d)")
 
 locations = [
     l1,
