b94690018196efe7afaba5f796f1d905b7142db9
ianstrom <iankstrom@gmail.com>
2023-03-28 21:03:04 -0400

    adding rest of ascii art, connecting to database

--- destination_instances.py
+++ destination_instances.py
@@ -5,33 +5,33 @@ from models import Destination
 destinations = [
     Destination(9, "./videos/dest_1.sh"),
     Destination(2, "./videos/dest_2.sh"),
-    Destination(1, ""),
-    Destination(3, ""),
+    Destination(1, "./videos/dest_3.sh"),
+    Destination(3, "./videos/dest_4.sh"),
     Destination(8, "./videos/dest_5.sh"),
 
-    Destination(2, ""),
-    Destination(4, ""),
-    Destination(6, ""),
-    Destination(5, ""),
-    Destination(3, ""),
+    Destination(2, "./videos/dest_6.sh"),
+    Destination(4, "./videos/dest_7.sh"),
+    Destination(6, "./videos/dest_8.sh"),
+    Destination(5, "./videos/dest_9.sh"),
+    Destination(3, "./videos/dest_10.sh"),
 
-    Destination(4, ""),
-    Destination(7, ""),
-    Destination(4, ""),
-    Destination(6, ""),
-    Destination(3, ""),
+    Destination(4, "./videos/dest_11.sh"),
+    Destination(7, "./videos/dest_12.sh"),
+    Destination(4, "./videos/dest_13.sh"),
+    Destination(6, "./videos/dest_14.sh"),
+    Destination(3, "./videos/dest_15.sh"),
 
     Destination(10, "./videos/dest_16.sh"),
-    Destination(1, ""),
+    Destination(1, "./videos/dest_17.sh"),
     Destination(14, "./videos/dest_18.sh"),
     Destination(11, "./videos/dest_19.sh"),
-    Destination(12, ""),
+    Destination(12, "./videos/dest_20.sh"),
 
-    Destination(9, ""),
+    Destination(9, "./videos/dest_21.sh"),
     Destination(10, "./videos/dest_22.sh"),
     Destination(13, "./videos/dest_23.sh"),
-    Destination(10, ""),
-    Destination(14, ""),
+    Destination(10, "./videos/dest_24.sh"),
+    Destination(14, "./videos/dest_25.sh"),
 
     Destination(12, "./videos/dest_26.sh"),
     Destination(15, "./videos/dest_27.sh"),
@@ -39,9 +39,8 @@ destinations = [
     Destination(12, "./videos/dest_29.sh"),
     Destination(16, "./videos/dest_30.sh"),
 
-    Destination(14, ""),
+    Destination(14, "./videos/dest_31.sh"),
     Destination(17, "./videos/dest_32.sh"),
     Destination(15, "./videos/dest_33.sh"),
     Destination(16, "./videos/dest_34.sh")
 ]
-
--- game.py
+++ game.py
@@ -3,35 +3,29 @@ import subprocess
 from character import Character
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import Path
-import random
+from models import Path, Person
 from moving import move_left, move_right, move_down, move_up
 
 engine = create_engine('sqlite:///billkills.db')
-
 Session = sessionmaker(bind=engine)
-
 session = Session()
 
-char = Character('fucked')
-muderer = random.randint(0,14)
+# char = Character('fucked')
 
+print("Choose a character: \n(1): Anson\n(2): Bill\n(3): Finn\n(4): Jack\n(5): Bobby\n(6): Brett\n(7): Chris C.\n(8): Chris W.\n(9): Eshwar\n(10): Jacob\n(11): Ian\n(12): Kyushik\n(13): Min\n(14): Michelle\n(15): Sally\n(16): Nick")
+x = input()
+character = session.query(Person).filter(Person.id == x).all()[0]
+char = Character(character.name)
 char.curr = session.query(Path).filter(Path.id == 1).all()[0]
 
 subprocess.run("./videos/loading-vid.sh")
-
 print(f'Reception: (a), Stairs: (d)')
-
-
-
-
 
 def on_press(key):
     if key.char == 'a':
         move_left(char)
 
     if key.char == 'd':
-
         move_right(char)
 
     if key.char == 'w':
